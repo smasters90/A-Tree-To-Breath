f44dbf96aa21393c35ea4b50826f407d9ac30694
Seba Masters <sebastian_masters@hotmail.com>
2023-07-29 21:29:38 -0300

    avances

--- reactFront/src/components/Main.jsx
+++ reactFront/src/components/Main.jsx
@@ -6,12 +6,13 @@ function Main() {
   return (
     <>
     <div className='d-flex flex-column flex-xl-row'>
-      <div className='d-flex justify-content-center col-xl-4'>
-        <img src={"/assets/logoMain2.png"} className='logo__main img-fluid' />
-      </div>
       <div className='d-flex flex-column'>
         <SobreNosotros />
-        <Boton className="boton__main" extension={`/calculadora/${1}`} nombre="Calcula tu impacto" clase="boton__main"/>
+        <Boton className="boton__main" extension={`/calculadora/${1}`} nombre="CALCULATE >" clase="boton__main"/>
+      </div>
+      <div className='cont__img d-flex flex column justify-content-center col-xl-4'>
+        <img src={"/assets/Steps_.png"} className='logo__main-steps img-fluid'/>
+        <img src={"/assets/GoGreenForest.png"} className='logo__main-tree img-fluid'/>
       </div>
     </div>
     
